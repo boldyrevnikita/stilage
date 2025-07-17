ce3a523a12392e782c8a03d909548f7150010faa
PnthrLeo <leon-1402@mail.ru>
2025-07-17 12:50:25 +0300

    fix rabbitMQ disconection issue

--- src/network/network.py
+++ src/network/network.py
@@ -2,6 +2,8 @@ import json
 from typing import Any, Callable, Optional, Union
 
 import pika
+import time
+from pika import exceptions
 
 
 class Sender:
@@ -9,7 +11,8 @@ class Sender:
                  port: Optional[Union[int, str]] = None,
                  vhost: Optional[str] = None,
                  credentials: Optional[pika.PlainCredentials] = None,
-                 heartbeat: int = 1800):
+                 heartbeat: int = 1800,
+                 reconnection_attempts: int = 5):
         self.queue_name = queue_name
         self.routing_key = routing_key
 
@@ -20,12 +23,29 @@ class Sender:
         if vhost is None:
             vhost = pika.ConnectionParameters._DEFAULT
 
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host=host, port=port, virtual_host=vhost, credentials=credentials,
-            heartbeat=heartbeat))
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.queue_name,
-                                   auto_delete=True)
+        self.host = host
+        self.port = port
+        self.vhost = vhost
+        self.credentials = credentials
+        self.heartbeat = heartbeat
+        self.reconnection_attempts = reconnection_attempts
+        self.connection = None
+        self.channel = None
+        self.connect()
+
+    def connect(self):
+        """Establish connection to the message queue.
+        """
+        try:
+            self.connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=self.host, port=self.port,
+                                          virtual_host=self.vhost,
+                                          credentials=self.credentials,
+                                          heartbeat=self.heartbeat))
+            self.channel = self.connection.channel()
+            self.channel.queue_declare(queue=self.queue_name, auto_delete=True)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def send(self, message: Any):
         """Send message to the queue.
@@ -33,8 +53,22 @@ class Sender:
         Args:
             message (Any): message to send
         """
-        self.channel.basic_publish(exchange='', routing_key=self.routing_key,
-                                   body=json.dumps(message))
+        attempts = 0
+
+        while attempts < self.reconnection_attempts:
+            try:
+                self.channel.basic_publish(exchange='',
+                                           routing_key=self.routing_key,
+                                           body=json.dumps(message))
+                break
+            except exceptions.StreamLostError:
+                print("Stream lost")
+            except Exception:
+                print("An error occurred while sending the message")
+            finally:
+                self.connect()
+                attempts += 1
+                time.sleep(1)
 
     def close(self):
         """Close the connection.
@@ -62,21 +96,42 @@ class MessageHandler:
         if vhost is None:
             vhost = pika.ConnectionParameters._DEFAULT
 
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host=host, port=port, virtual_host=vhost, credentials=credentials,
-            heartbeat=heartbeat))
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.queue_name,
-                                   auto_delete=True)
+        self.host = host
+        self.port = port
+        self.vhost = vhost
+        self.credentials = credentials
+        self.heartbeat = heartbeat
+        self.connection = None
+        self.channel = None
+        self.connect()
+
+    def connect(self):
+        """Establish connection to the message queue.
+        """
+        try:
+            self.connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=self.host, port=self.port,
+                                          virtual_host=self.vhost,
+                                          credentials=self.credentials,
+                                          heartbeat=self.heartbeat))
+            self.channel = self.connection.channel()
+            self.channel.queue_declare(queue=self.queue_name, auto_delete=True)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def receive(self):
         """Start receiving messages from the queue.
         """
-        self.channel.basic_consume(
-            queue=self.queue_name,
-            on_message_callback=self.callback,
-            auto_ack=True)
-        self.channel.start_consuming()
+        while True:
+            try:
+                self.channel.basic_consume(
+                    queue=self.queue_name,
+                    on_message_callback=self.callback,
+                    auto_ack=True)
+                self.channel.start_consuming()
+                break
+            except exceptions.StreamLostError:
+                self.connect()
 
     def callback(self, ch, method, properties, body):
         """Callback function for message processing.
