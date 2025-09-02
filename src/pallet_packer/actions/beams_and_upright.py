9bcefe4bf6fbb5a23b966e0c9b451479765d02a8
rareHalex <v.kalinin@g.nsu.ru>
2025-09-02 15:46:49 +0000

    Edit beams_and_upright.py

--- src/pallet_packer/actions/beams_and_upright.py
+++ src/pallet_packer/actions/beams_and_upright.py
@@ -82,7 +82,7 @@ def _find_suitable_upright_type(
     pallet: Pallet,
     beam_type: BeamType
 ) -> tuple[UprightType, float, float, float]:
-    available_height = available_zone.height - max(reference_book.frame_height_eps, available_height % 500)
+    available_height = available_zone.height - max(reference_book.frame_height_eps, available_zone.height % 500)
 
     pallet_extra_space = reference_book.frame_height2pallet_extra_space[-1][1]
     for frame_height, extra_space in \
